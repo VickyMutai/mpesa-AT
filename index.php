2aeb25e56eb931c79b80ec9170a8c135a902c04d
“VickyMutai” <vicky.mutai96@gmail.com>
2021-12-29 21:04:17 +0300

    initital commit

---
+++ index.php
@@ -0,0 +1,44 @@
+include_once 'menu.php';
+//set isUserRegistered flag to true
+$isUserRegistered = true;
+//Read the data sent via POST from our AT API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];`
+<?php
+$menu = new Menu();
+    if($text == "" && $isUserRegistered == true){
+         //user is registered and string is is empty
+        echo "CON " . $menu->mainMenuRegistered("<Add a name here>");
+    }else if($text == "" && $isUserRegistered== false){
+         //user is unregistered and string is is empty
+         $menu->mainMenuUnRegistered();
+    }else if($isUserRegistered== false){
+        //user is unregistered and string is not empty
+        $textArray = explode("*", $text);
+        switch($textArray[0]){
+            case 1: 
+                $menu->registerMenu($textArray, $phoneNumber);
+            break;
+            default:
+                echo "END Invalid choice. Please try again";
+        }
+    }else{
+        //user is registered and string is not empty
+        $textArray = explode("*", $text);
+        switch($textArray[0]){
+            case 1: 
+                $menu->sendMoneyMenu($textArray,$sessionId);
+            break;
+            case 2: 
+                $menu->withdrawMoneyMenu($textArray);
+            break;
+            case 3:
+                $menu->checkBalanceMenu($textArray);
+                break;
+            default:
+                echo "END Inavalid menu\n";
+        }
+    }
+?>
---
+++ util.php
@@ -0,0 +1,5 @@
+class Util{
+        static $GO_BACK = "98";        
+        static $GO_TO_MAIN_MENU = "99";
+        static $COUNTRY_CODE = "+254";        
+    }
