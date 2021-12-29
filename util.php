92fbdb85674cd1ddd1634db0a0be95091a193c85
“VickyMutai” <vicky.mutai96@gmail.com>
2021-12-29 22:08:34 +0300

    This is a trial

--- util.php
+++ util.php
@@ -2,4 +2,6 @@ class Util{
         static $GO_BACK = "98";        
         static $GO_TO_MAIN_MENU = "99";
         static $COUNTRY_CODE = "+254";        
+        static  $API_KEY = "a1a0c3c8acc14cd2a855789922e92a987ea7f02c5a77bfc07fd9621088afd9d4"; //sandbox
+        static $API_USERNAME = "sandbox"; //sandbox
     }
